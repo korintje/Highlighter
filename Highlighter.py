5fb74a33e8a6b037bcdd0ca9f7e39f915cbf78ac
chakuro <t.hosomi1410@gmail.com>
2022-12-18 00:07:42 +0900

    implement Custom color

--- Highlighter.py
+++ Highlighter.py
@@ -20,7 +20,7 @@ handlers = []
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 RESOURCE_DIR = os.path.join(CURRENT_DIR, 'resources')
 APPEARANCE_LIB_ID = 'BA5EE55E-9982-449B-9D66-9F036540E140'
-DEFAULT_SCALE = 0.03
+DEFAULT_SCALE = 0.02
 DEFAULT_OPACITY = 0.5
 TLSTYLE = core.DropDownStyles.TextListDropDownStyle
 NBFEATURE = fusion.FeatureOperations.NewBodyFeatureOperation
@@ -33,7 +33,14 @@ _options = _cfg["options"]
 DEFAULT_COLOR_NAME = _options.get("default_value")
 APPEARANCE_LIB_ID = _options.get("appearance_lib_id")
 APPEARANCE_ID = _options.get("appearance_id")
-HIGHLIGHT_COLORS = _cfg["rgbs"]
+HIGHLIGHT_COLORS = {}
+try:
+    for key, value in _cfg["rgbs"].items():
+        HIGHLIGHT_COLORS[key] = [int(rgb.strip()) for rgb in value.strip().split(",")]
+    HIGHLIGHT_COLORS["custom color"] = [128, 128, 128]
+except:
+    if ui:
+        ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 # Execute command handler
 class CommandExecuteHandler(core.CommandEventHandler):
@@ -47,18 +54,33 @@ class CommandExecuteHandler(core.CommandEventHandler):
             command = args.firingEvent.sender
             inputs = command.commandInputs
             highlight = Highlight()
+            isCustomColorUsed = False
             for ipt in inputs:
                 if ipt.id == "selectedObjects":
                     obj_count = ipt.selectionCount
                     for i in range(obj_count):
                         selection = ipt.selection(i)
                         highlight.add_body(selection.entity)
-                if ipt.id == "offset":
+                elif ipt.id == "offset":
                     highlight.set_offset(ipt.value)
-                if ipt.id == "colorName":
-                    highlight.set_color_name(ipt.selectedItem.name)
-                if ipt.id == "opacity":
+                elif ipt.id == "opacity":
                     highlight.set_opacity(ipt.value)
+                elif ipt.id == "colorName":
+                    color_name = ipt.selectedItem.name
+                    isCustomColorUsed = True if color_name == "custom color" else False
+                    highlight.set_color_name(color_name)
+                elif ipt.id == "red":
+                    ipt.isEnabled = isCustomColorUsed
+                    HIGHLIGHT_COLORS["custom color"][0] = ipt.value
+                    ipt.value = HIGHLIGHT_COLORS[color_name][0]
+                elif ipt.id == "green":
+                    ipt.isEnabled = isCustomColorUsed
+                    HIGHLIGHT_COLORS["custom color"][1] = ipt.value
+                    ipt.value = HIGHLIGHT_COLORS[color_name][1]
+                elif ipt.id == "blue":
+                    ipt.isEnabled = isCustomColorUsed
+                    HIGHLIGHT_COLORS["custom color"][2] = ipt.value
+                    ipt.value = HIGHLIGHT_COLORS[color_name][2]
             highlight.build()
             args.isValidResult = True
 
@@ -107,14 +129,17 @@ class CommandCreatedHandler(core.CommandCreatedEventHandler):
             cmd.destroy.add(onDestroy)
             handlers.append(onDestroy)
             
-            # Add inputs of molecular settings           
+            # Add inputs of molecular settings
             inputs: adsk.core.CommandInputs = cmd.commandInputs
             selIpt = inputs.addSelectionInput('selectedObjects', 'Select objects', 'select target object')
             selIpt.setSelectionLimits(0)
             selIpt.addSelectionFilter("SolidBodies")
 
             # Add inputs of offset
-            inputs.addFloatSpinnerCommandInput("offset", "Offset", "", 0.0, 100.0, 0.01, DEFAULT_SCALE)
+            inputs.addFloatSpinnerCommandInput("offset", "Offset", "cm", 0.0, 100.0, 0.01, DEFAULT_SCALE)
+
+            # Add inputs of opacity
+            inputs.addFloatSpinnerCommandInput("opacity", "Opacity", "", 0.0, 1.0, 0.1, DEFAULT_OPACITY)
 
             # Add inputs of highlight color name
             color_input = inputs.addDropDownCommandInput("colorName", "Color name", TLSTYLE)
@@ -122,8 +147,10 @@ class CommandCreatedHandler(core.CommandCreatedEventHandler):
                 is_selected = True if color_name == DEFAULT_COLOR_NAME else False
                 color_input.listItems.add(color_name, is_selected)
 
-            # Add inputs of opacity
-            inputs.addFloatSpinnerCommandInput("opacity", "Opacity", "", 0.0, 1.0, 0.1, DEFAULT_OPACITY)
+            # Add inputs of custom RGB if Custom color is selected
+            inputs.addIntegerSpinnerCommandInput("red", "R", 0, 255, 1, 128)
+            inputs.addIntegerSpinnerCommandInput("green", "G", 0, 255, 1, 128)
+            inputs.addIntegerSpinnerCommandInput("blue", "B", 0, 255, 1, 128)
 
         except:
             if ui:
@@ -180,7 +207,8 @@ class Highlight:
 
             # Set appearance
             highlight_color_name = f'highlight_{self.color_name}'
-            color = [int(rgb.strip()) for rgb in HIGHLIGHT_COLORS[self.color_name].strip().split(",")]
+            # color = [int(rgb.strip()) for rgb in HIGHLIGHT_COLORS[self.color_name].strip().split(",")]
+            color = HIGHLIGHT_COLORS[self.color_name]
             try:
                 highlightColor = favoriteAppearances.itemByName(highlight_color_name)
             except:
